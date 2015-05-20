17547a17b481c6254c5931c92bc73c3258047a9d
Mathieu Ducharme <mat@locomotive.ca>
2015-05-19 20:56:06 -0400

    Default index

--- www/index.php
+++ www/index.php
@@ -1,39 +1,42 @@
 <?php
+// From `charcoal-core`
 use \Charcoal\Charcoal as Charcoal;
-use \Charcoal\Admin\Module as AdminModule;
+// From `charcoal-base`
 use \Charcoal\Template\TemplateView as TemplateView;
+// From `charcoal-admin
+use \Charcoal\Admin\Module as AdminModule;
 
 include '../vendor/autoload.php';
 
 // Setup configuration and initialize Charcoal
 Charcoal::init([
-	'config'=>'../config/config.php'
+    'config'=>'../config/config.php'
 ]);
 
-$admin_path = 'admin';
-
 // Handle admin request
-Charcoal::app()->group('/admin', function()  {
-	AdminModule::init([
-		'config'=>[
-			'base_path'=>'admin'
-		]
-	]);
+Charcoal::app()->group('/'.Charcoal::config('admin_path'), function()  {
+    //var_dump('...');
+    $admin_module = new AdminModule([
+        'config'=>[
+            'base_path'=>Charcoal::config('admin_path')
+        ]
+    ]);
+    $admin_module->setup_routes();
 });
 
 // Default template, by default
 Charcoal::app()->get('/:actions+?', function ($actions=['home']) {
-	$template = implode('/', $actions);
-    $view = new TemplateView;
+    $template = implode('/', $actions);
+    $view = new TemplateView();
     $view->from_ident('boilerplate/template/'.$template);
-	$content = $view->render();
+    $content = $view->render();
 
-	if($content) {
-		echo $content;
-	}
-	else {
-		Charcoal::app()->halt(404, 'Page not found');
-	}
+    if($content) {
+        echo $content;
+    }
+    else {
+        Charcoal::app()->halt(404, 'Page not found');
+    }
 });
 
 Charcoal::app()->run();
