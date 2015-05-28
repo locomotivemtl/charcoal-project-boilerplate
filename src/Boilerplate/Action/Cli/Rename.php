146e0d2d0ec2b22fc073a777dd8cdab1fef20da0
Mathieu Ducharme <mat@locomotive.ca>
2015-05-28 07:43:34 -0400

    Fix rename script

--- src/Boilerplate/Action/Cli/Rename.php
+++ src/Boilerplate/Action/Cli/Rename.php
@@ -6,6 +6,21 @@ use \InvalidArgumentException as InvalidArgumentException;
 
 use \Charcoal\Action\CliAction as CliAction;
 
+if (!function_exists('glob_recursive')) {
+    function glob_recursive($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern).'/*', (GLOB_ONLYDIR|GLOB_NOSORT)) as $dir) {
+            $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+        }
+        return $files;
+        
+    }
+}
+
+/**
+*
+*/
 class Rename extends CliAction
 {
 
@@ -74,6 +89,7 @@ class Rename extends CliAction
         $climate->arguments->parse();
         $project_name = $climate->arguments->get('project_name');
         $verbose = !!$climate->arguments->get('quiet');
+        $this->set_verbose($verbose);
 
         if (!$project_name) {
             $input = $climate->input('What is the name of the project?');
@@ -83,17 +99,6 @@ class Rename extends CliAction
             $this->set_project_name($project_name);
         } catch (\Exception $e) {
             $climate->error($e->getMessage());
-        }
-
-        if (!function_exists('glob_recursive')) {
-            function glob_recursive($pattern, $flags = 0)
-            {
-                $files = glob($pattern, $flags);
-                foreach (glob(dirname($pattern).'/*', (GLOB_ONLYDIR|GLOB_NOSORT)) as $dir) {
-                    $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
-                }
-                return $files;
-            }
         }
 
         $climate->bold()->out(sprintf('Using "%s" as project name...', $project_name));
@@ -111,21 +116,32 @@ class Rename extends CliAction
     {
         $climate = $this->climate();
         $project_name = $this->project_name();
+        $verbose = $this->verbose();
 
         $climate->out("\n".'Replacing file content...');
-        foreach (glob_recursive("www/*") as $filename) {
-            if (!is_dir($filename)) {
-                $file = file_get_contents($filename);
-                $num_replacement1 = 0;
-                $num_replacement2 = 0;
-                $content = preg_replace("/boilerplate/", $project_name, $file, -1, $num_replacement1);
-                $content = preg_replace("/Boilerplate/", ucfirst($project_name), $content, -1, $num_replacement2);
-                $num_replacements = ($num_replacement1+$num_replacement2);
-                if ($num_replacements > 0) {
-                    //file_put_contents($filename, $content);
-                    if ($verbose) {
-                        $climate->dim()->out(sprintf('%d occurence(s) of "boilerplate" has been changed to "%s" in file "%s"', $num_replacements, $project_name, $filename));
-                    }
+        $files = array_merge(
+            glob_recursive('config/*'),
+            glob_recursive('metadata/*'),
+            glob_recursive('src/*'),
+            glob_recursive('templates/*'),
+            glob_recursive('tests/*'),
+            glob_recursive('www/*'),
+            glob('*.*')
+        );
+        foreach ($files as $filename) {
+            if (is_dir($filename)) {
+                continue;
+            }
+            $file = file_get_contents($filename);
+            $num_replacement1 = 0;
+            $num_replacement2 = 0;
+            $content = preg_replace("/boilerplate/", $project_name, $file, -1, $num_replacement1);
+            $content = preg_replace("/Boilerplate/", ucfirst($project_name), $content, -1, $num_replacement2);
+            $num_replacements = ($num_replacement1+$num_replacement2);
+            if ($num_replacements > 0) {
+                //file_put_contents($filename, $content);
+                if ($verbose) {
+                    $climate->dim()->out(sprintf('%d occurence(s) of "boilerplate" has been changed to "%s" in file "%s"', $num_replacements, $project_name, $filename));
                 }
             }
         }
@@ -133,8 +149,12 @@ class Rename extends CliAction
 
     protected function rename_files()
     {
+        $climate = $this->climate();
+        $project_name = $this->project_name();
+        $verbose = $this->verbose();
+
         $climate->out("\n".'Renaming files and directories');
-        $boilerplate_files = glob_recursive("www/*boilerplate*");
+        $boilerplate_files = glob_recursive("*boilerplate*");
         $boilerplate_files = array_reverse($boilerplate_files);
         foreach ($boilerplate_files as $filename) {
             $target_name = preg_replace("/boilerplate/", $project_name, basename($filename));
@@ -148,7 +168,7 @@ class Rename extends CliAction
 
         }
 
-        $boilerplate_files = glob_recursive("www/*Boilerplate*");
+        $boilerplate_files = glob_recursive("*Boilerplate*");
         $boilerplate_files = array_reverse($boilerplate_files);
         foreach ($boilerplate_files as $filename) {
             $climate->inline('.');
