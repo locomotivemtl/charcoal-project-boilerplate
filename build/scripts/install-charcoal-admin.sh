1850dadcdbce348be9e8156c4246d337296a55b4
Mathieu Ducharme <mat@locomotive.ca>
2017-06-01 10:52:22 -0400

    Charcoal admin quick-n-dirty install script (bash)

---
+++ build/scripts/install-charcoal-admin.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+composer install
+
+cd vendor/locomotivemtl/charcoal-admin
+
+composer install
+npm install
+bower install
+
+grunt --force
+
+cd ../../..
+
+echo -n "Do you want to add the admin assets to the project's git repository? "
+read gitadd
+if [ "$gitadd" == "y" ]; then
+    git add www/assets/admin
+    git commit -m "Auto-generated admin assets"
+    echo -n "Do you want to push the admin assets to remote's git repository? "
+    read gitpush
+    if [ "$gitpush" == "y" ]; then
+        git push
+    fi
+fi
+
+echo -n "Do you want to add an administrator user? "
+read adminadd
+if [ "$adminadd" == "y" ]; then
+    ./vendor/bin/charcoal admin/user/create
+else
+    echo -n "You can create a new administrator user later by running \"./vendor/bin/charcoal admin/user/create\""
+fi
