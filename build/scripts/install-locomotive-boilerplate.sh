e77fe1b0ac0ea0272735c9ac007c80d31f95addc
Mathieu Ducharme <mat@locomotive.ca>
2017-06-21 12:11:36 -0400

    Crude script to install locomotive-boilerplate.

---
+++ build/scripts/install-locomotive-boilerplate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone https://github.com/locomotivemtl/locomotive-boilerplate
+
+rm -rf locomotive-boilerplate/.git
+
+rm locomotive-boilerplate/www/index.html;
+rm locomotive-boilerplate/LICENSE;
+rm locomotive-boilerplate/.editorconfig;
+rm locomotive-boilerplate/.gitignore;
+mv locomotive-boilerplate/README.md locomotive-boilerplate/README-locomotive.md;
+
+cp -Rav locomotive-boilerplate/* .
+cp -Rav locomotive-boilerplate/.* .
+
+rm -rf locomotive-boilerplate
+
+ncu -u
+npm install
