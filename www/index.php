318a91752df623ff513dd016f293aec5dbd11c8f
Mathieu Ducharme <mat@locomotive.ca>
2017-11-21 18:31:31 -0500

    Simplify front controller. Do not session_start() on every request.

--- www/index.php
+++ www/index.php
@@ -1,28 +1,10 @@
 <?php
 
-/**
- * Charcoal - A PHP Framework
- *
- * @package   Charcoal
- * @author    Locomotive <info@locomotive.ca>
- * @copyright Copyright (c) 2017 Locomotive Inc.
- * @link      https://github.com/locomotivemtl/charcoal-project-boilerplate
- */
-
-/** Built on top of Slim */
 use Charcoal\App\App;
 use Charcoal\App\AppConfig;
 use Charcoal\App\AppContainer;
 
-/** If we're not using PHP 5.6+, explicitly set the default character set. */
-if (PHP_VERSION_ID < 50600) {
-    ini_set('default_charset', 'UTF-8');
-}
-
-/**
- * If you are using PHP's built-in server, return FALSE
- * for existing files on the filesystem.
- */
+/* If using PHP's built-in server, return false to skip existing files on the filesystem. */
 if (PHP_SAPI === 'cli-server') {
     $file = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 
@@ -31,30 +13,17 @@ if (PHP_SAPI === 'cli-server') {
     }
 }
 
-/** Register the Composer autoloader */
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-/** Start new or resume existing session */
-if (!session_id()) {
-    session_start();
-}
-
-/** Import the application's settings */
+/* Import the application's settings */
 $appConfig = new AppConfig();
 $appConfig->addFile(dirname(__DIR__) . '/config/config.php');
 
-/** Build the DI container */
+/* Build the DI container */
 $container = new AppContainer([
-    'config'   => $appConfig,
-    'settings' => [
-        'displayErrorDetails' => true
-    ]
+    'config'   => $appConfig
 ]);
 
-/** Instantiate a Charcoal~Slim application */
+/* Instantiate a Charcoal~Slim application and run */
 $app = App::instance($container);
-
-/**
- * Run The Application
- */
 $app->run();
