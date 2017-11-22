d9dc2d7deaa43c2a0b7c2fd010d82631deb72e9f
Mathieu Ducharme <mat@locomotive.ca>
2017-11-21 19:08:07 -0500

    Do not set error_reporting or display_errors in front controller.

--- config/config.php
+++ config/config.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Load Application Configuration
- *
- * Bootstrap the given application.
- *
- * @package Charcoal
- */
-
-/** For the time being, let's track and show all issues. */
-error_reporting(E_ALL);
 
 /** The application's absolute root path */
 $this['base_path'] = dirname(__DIR__).'/';
@@ -29,20 +19,12 @@ $this->addFile(__DIR__ . '/routes.json');
 $this->addFile(__DIR__ . '/redirections.json');
 
 
-/**
- * Load environment settings; such as database credentials
- * or credentials for 3rd party services.
- */
-$app_environment = getenv('APPLICATION_ENV') ?: 'local';
+$environment = getenv('APPLICATION_ENV') ?: 'local';
 
-if ($app_environment) {
-    $environment = preg_replace('/[^A-Za-z0-9_]+/', '', $app_environment);
-
-    /** Import local settings */
-    $cfg = sprintf('%1$s/config.%2$s.json', __DIR__, $environment);
-    if (file_exists($cfg)) {
-        $this->addFile($cfg);
-    }
+/** Import local settings */
+$cfg = sprintf('%1$s/config.%2$s.json', __DIR__, $environment);
+if (file_exists($cfg)) {
+    $this->addFile($cfg);
 }
 
-ini_set('display_errors', $this['debug']);
+
