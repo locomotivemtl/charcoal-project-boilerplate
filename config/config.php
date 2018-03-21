0ddfb5707e6e67883c95c213d58344332ab50510
Chauncey McAskill <chauncey@locomotive.ca>
2018-03-21 14:38:10 -0400

    Amend 2c862755: Fix admin file for latest admin

--- config/config.php
+++ config/config.php
@@ -6,9 +6,6 @@ $this['base_path'] = dirname(__DIR__).'/';
 /** Import core settings */
 $this->addFile(__DIR__ . '/config.json');
 
-/** Admin settings */
-$this->addFile(__DIR__ . '/admin.json');
-
 /** Import Boilerplate middlewares */
 $this->addFile(__DIR__ . '/middlewares.json');
 
@@ -18,10 +15,10 @@ $this->addFile(__DIR__ . '/routes.json');
 /** Import Boilerplate redirections */
 $this->addFile(__DIR__ . '/redirections.json');
 
-
 $environment = getenv('APPLICATION_ENV') ?: 'local';
 
 /** Import local settings */
+
 $cfg = sprintf('%1$s/config.%2$s.json', __DIR__, $environment);
 if (file_exists($cfg)) {
     $this->addFile($cfg);
