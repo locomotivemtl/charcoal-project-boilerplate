0b5143c63c609931d32b7da19291fdcf555e9375
Chauncey McAskill <chauncey@locomotive.ca>
2021-04-07 12:39:09 -0400

    Remove redundant providers from AppServiceProvider
    `CacheServiceProvider` and `TranslatorServiceProvider` are registered by locomotivemtl/charcoal-app and `UiServiceProvider` is registered by locomotivemtl/charcoal-admin.
    
    If authentication is needed outside of the Admin area, then `AuthServiceProvider` should be registered directly.

--- src/App/ServiceProvider/AppServiceProvider.php
+++ src/App/ServiceProvider/AppServiceProvider.php
@@ -6,20 +6,11 @@ namespace App\ServiceProvider;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
-// From 'charcoal-cache'
-use Charcoal\Cache\ServiceProvider\CacheServiceProvider;
-
 // From 'charcoal-email'
 use Charcoal\Email\ServiceProvider\EmailServiceProvider;
 
 // From 'charcoal-core'
 use Charcoal\Model\ServiceProvider\ModelServiceProvider;
-
-// From 'charcoal-translator'
-use Charcoal\Translator\ServiceProvider\TranslatorServiceProvider;
-
-// From 'charcoal-ui'
-use Charcoal\Ui\ServiceProvider\UiServiceProvider;
 
 /**
  * App Service Provider
@@ -32,11 +23,8 @@ class AppServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
-        $container->register(new CacheServiceProvider());
         $container->register(new EmailServiceProvider());
         $container->register(new ModelServiceProvider());
-        $container->register(new TranslatorServiceProvider());
-        $container->register(new UiServiceProvider());
 
         $container->extend('view/mustache/helpers', function (array $helpers, Container $container) {
             unset($container);
